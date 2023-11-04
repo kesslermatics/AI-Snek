422e46759aff46ca3990a7317965918c5e739688
Robert Kessler <robby9903@gmail.com>
2023-11-04 17:52:08 +0100

    Added terminal visualization

--- environments/snake_env.py
+++ environments/snake_env.py
@@ -2,24 +2,30 @@ import numpy as np
 import gym
 from gym import spaces
 from random import randrange
+import os
+import time
 
 class SnakeEnv(gym.Env):
     def __init__(self):
         super(SnakeEnv, self).__init__()
-        self.food = np.array([0, 0])
-        self.snake = [np.array([10, 0])]
-        self.aim = np.array([0, -10])
+        self.snake = [np.array([10, 10])]
+        self.aim = np.array([0, 1])
         self.grid_size = 20
+        self.food = np.array([randrange(0, self.grid_size), randrange(0, self.grid_size)])
         self.reward = 0
+        self.step_count = 0
+        self.max_steps = 200
+        self.start_time = time.time()
         self.done = False
-        self.action_space = spaces.Discrete(4)  # Up, Down, Left, Right
+        self.action_space = spaces.Discrete(3) # Left, Forward, Right
         self.observation_space = spaces.Box(low=0, high=self.grid_size, shape=(self.grid_size, self.grid_size), dtype=np.int_)
 
     def reset(self):
-        self.food = np.array([0, 0])
-        self.snake = [np.array([10, 0])]
-        self.aim = np.array([0, -10])
+        print("Reset")
+        self.snake = [np.array([10, 10])]
+        self.aim = np.array([0, -1])
         self.done = False
+        self.start_time = time.time()
         return self._get_state()
 
     def _get_state(self):
@@ -29,29 +35,73 @@ class SnakeEnv(gym.Env):
         state[self.food[0]][self.food[1]] = 2
         return state
 
-    def step(self, action):
-        if action == 0:  # Right
-            self.aim = np.array([10, 0])
-        elif action == 1:  # Left
-            self.aim = np.array([-10, 0])
-        elif action == 2:  # Up
-            self.aim = np.array([0, 10])
-        elif action == 3:  # Down
-            self.aim = np.array([0, -10])
+    def step(self, action, episode, current_reward):
+        reward_for_eating = 10
+        reward_for_dying = -10
+        reward_for_moving = 1
+
+        self.step_count += 1
+
+        # Aktualisierung der Bewegungsrichtung basierend auf der aktuellen Richtung und der ausgewählten Aktion
+        if np.array_equal(self.aim, [0, 1]):  # moving up
+            if action == 0:  # turn left
+                self.aim = np.array([-1, 0])
+            elif action == 2:  # turn right
+                self.aim = np.array([1, 0])
+        elif np.array_equal(self.aim, [0, -1]):  # moving down
+            if action == 0:  # turn left
+                self.aim = np.array([1, 0])
+            elif action == 2:  # turn right
+                self.aim = np.array([-1, 0])
+        elif np.array_equal(self.aim, [-1, 0]):  # moving left
+            if action == 0:  # turn left
+                self.aim = np.array([0, -1])
+            elif action == 2:  # turn right
+                self.aim = np.array([0, 1])
+        elif np.array_equal(self.aim, [1, 0]):  # moving right
+            if action == 0:  # turn left
+                self.aim = np.array([0, 1])
+            elif action == 2:  # turn right
+                self.aim = np.array([0, -1])
 
         head = self.snake[-1] + self.aim
         if not self._inside(head) or any((segment == head).all() for segment in self.snake):
             self.done = True
-            self.reward = -10
+            self.reward = reward_for_dying 
         else:
             if (head == self.food).all():
-                self.reward = 10
+                self.step_count = 0
+                self.reward = reward_for_eating 
                 self.snake.append(head)
-                self.food = np.array([randrange(0, self.grid_size), randrange(0, self.grid_size)])
+                while True:
+                    self.food = np.array([randrange(0, self.grid_size), randrange(0, self.grid_size)])
+                    if not any((segment == self.food).all() for segment in self.snake):
+                        break
             else:
                 self.snake.append(head)
                 self.snake.pop(0)
-                self.reward = 1
+                self.reward = reward_for_moving
+        
+        if (self.step_count >= self.max_steps):
+            self.done = True
+            self.reward = reward_for_dying
+            self.step_count = 0
+        
+        os.system('cls' if os.name == 'nt' else 'clear') 
+        print(f"Episode: {episode}")
+        print(f"Food: {self.food}")
+        print(f"Snake: {self.snake}")
+        print(f"Elapsed Time: {str(round(time.time() - self.start_time, 2))}s")
+        print(f"Current Reward: {current_reward}")
+        grid_display = [[' ' for _ in range(self.grid_size)] for _ in range(self.grid_size)]
+        
+        for segment in self.snake:
+            grid_display[segment[0]][segment[1]] = 'S'
+        
+        grid_display[self.food[0]][self.food[1]] = 'F'
+        
+        for row in grid_display:
+            print(' '.join(row))
 
         return self._get_state(), self.reward, self.done, {}
 
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from environments.snake_env import SnakeEnv
 from agents.snake_agent import DQNAgent
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+plt.ion() 
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.set_title('Training Progress')
+ax.set_xlabel('Episode')
+ax.set_ylabel('Score')
+xs, ys = [], []
 
 env = SnakeEnv()
 state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
 action_dim = env.action_space.n
 agent = DQNAgent(state_dim, action_dim)
 
-episodes = 50
+output = []
+
+episodes = 200
 for e in range(episodes):
+    start_time = time.time()
     state = env.reset()
     episode_reward = 0
 
     while True:
         action = agent.act(state)
-        next_state, reward, done, _ = env.step(action)
+        next_state, reward, done, _ = env.step(action, episode=e, current_reward=episode_reward)
         
         agent.remember(state, action, reward, next_state, done)
         agent.train()
@@ -22,6 +35,19 @@ for e in range(episodes):
         episode_reward += reward
 
         if done:
-            print(f"Episode: {e+1}/{episodes}, Score: {episode_reward}")
+            end_time = time.time()
+            output.append(f"Episode: {e+1}/{episodes}, Score: {episode_reward}, Elapsed time: {str(round(end_time - start_time, 2))}s")
             agent.update_target_model()
+
+            xs.append(e+1)
+            ys.append(episode_reward)
+        
+            ax.plot(xs, ys, color='blue')
+            plt.draw()
+        
+            clear_output(wait=True)
             break
+
+for text in output:
+    print(text)
+plt.show(block=True)
